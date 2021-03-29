d0b0aae137752882c668c1de2a6bbf6fa5acf8da
Jesse Wenger <wengerj@gmail.com>
2021-03-29 13:03:04 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.string :still_in_business
+      t.boolean :still_in_business
       t.time :opening_time
       t.time :closing_time
     end
