adf76c2498cdf05d2f37e2c6d442135107e0def2
Jesse Wenger <wengerj@gmail.com>
2021-03-29 12:26:50 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.text :name
+      t.text :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.text :business_status
+      t.time :opening_time
+      t.time :closing_time
+    end
+  end
+end
